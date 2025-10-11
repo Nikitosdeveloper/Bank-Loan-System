572fd27cea04bc2cd23ea788a4cb33c6d4714ec6
Nikita <yakovnikita01@gmail.com>
2025-10-11 16:19:01 +0300

    init commit

---
+++ src/main/java/com/busir/gardarian/bankloansystem/BankLoanSystemApplication.java
@@ -0,0 +1,13 @@
+package com.busir.gardarian.bankloansystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankLoanSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankLoanSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/busir/gardarian/bankloansystem/BankLoanSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.busir.gardarian.bankloansystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankLoanSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
