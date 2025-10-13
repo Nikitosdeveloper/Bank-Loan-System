79cfacf590fe08c20f2780ed79faad36f3ca1ca7
Nikita <yakovnikita01@gmail.com>
2025-10-13 17:41:01 +0300

    Change type of id, in Interface and stub class UserRepository, from int to Long

--- src/main/java/com/busir/gardarian/bankloansystem/service/interfaces/UserRepositoryImpl.java
+++ src/main/java/com/busir/gardarian/bankloansystem/service/interfaces/UserRepositoryImpl.java
@@ -3,7 +3,7 @@ package com.busir.gardarian.bankloansystem.service.interfaces;
 import com.busir.gardarian.bankloansystem.entity.User;
 
 public interface UserRepositoryImpl {
-    public User findById(int id);
+    public User findById(Long id);
     public User findByUsername(String username);
     public User findByEmail(String email);
     public User save(User user);
--- src/main/java/com/busir/gardarian/bankloansystem/service/stub/StubUserRepository.java
+++ src/main/java/com/busir/gardarian/bankloansystem/service/stub/StubUserRepository.java
@@ -14,7 +14,7 @@ import java.time.Instant;
 public class StubUserRepository implements UserRepositoryImpl {
 
     @Override
-    public User findById(int id) {
+    public User findById(Long id) {
         return new User(
                 3L,
                 "testemail@gmail.com",
