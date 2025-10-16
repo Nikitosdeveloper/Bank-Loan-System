6f8a70f1b24d04b8860b38af15dc178525e82ad1
Nikita <yakovnikita01@gmail.com>
2025-10-16 20:12:21 +0300

    Add new entity ClientAdditionalInfo.java

---
+++ src/main/java/com/busir/gardarian/bankloansystem/entity/ClientAdditionalInfo.java
@@ -0,0 +1,12 @@
+package com.busir.gardarian.bankloansystem.entity;
+
+import java.math.BigDecimal;
+
+public class ClientAdditionalInfo {
+    private Long id;
+    private Long userId;
+    private Integer age;
+    private BigDecimal monthlyIncome;
+    private BigDecimal otherIncome;
+    private BigDecimal totalIncome;
+}
