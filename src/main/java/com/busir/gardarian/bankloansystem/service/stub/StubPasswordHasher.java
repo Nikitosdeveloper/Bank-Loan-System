f0f41e9a0c43d93b5aa773ef16a1754b271637a0
Nikita <yakovnikita01@gmail.com>
2025-10-26 01:45:15 +0300

    fixed profiles

--- src/main/java/com/busir/gardarian/bankloansystem/service/stub/StubPasswordHasher.java
+++ src/main/java/com/busir/gardarian/bankloansystem/service/stub/StubPasswordHasher.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile({"devService", "devDAO"})
+@Profile({"devService"})
 public class StubPasswordHasher implements PasswordHasherImpl {
     @Override
     public String hashPassword(String password) {
